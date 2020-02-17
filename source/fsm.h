f20db536ea81b0a0c7b3f0f8d89b648803b5fd48
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2020-02-17 16:41:29 +0100

    Add fsm.h

---
+++ source/fsm.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief Finite state machine controlling system.
+ *
+ */
+
+/**
+ * @brief Enum of the possible defined states
+ */
+enum 
+
+
+
+/**
+ * @brief 
+ *
+ */
+void fsmUpdate(State: currentState);
