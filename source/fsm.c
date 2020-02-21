89f5901bc9988fb865f767384c893aa97b8cae11
Simen Gangstad <simen.gangstad@me.com>
2020-02-22 00:08:41 +0100

    Move main funtion to fsm

--- source/fsm.c
+++ source/fsm.c
@@ -3,13 +3,48 @@
  */
 
 #include <stdbool.h>
+#include <stdio.h>
 
-void fsmUpdate(State currentState) {
+#include "fsm.h"
 
-	State nextState = fsmDecideNextState(currentState);
+int main() {
 
+    int error = hardware_init();
+    if(error != 0){
+        fprintf(stderr, "Unable to initialize hardware\n");
+        exit(1);
+    }
 
-	return;
+	State currentState = Startup;
+
+	// Transition to the start up state so we enter it properly before the main loop
+	fsmTransition(Undefined, currentState);
+
+    while(1) {
+		State previousState = currentState;
+		State nextState = fsmDecideNextState(currentState);	
+
+		if (nextState != previousState) {
+			fsmTransition(currentState, nextState);
+		}
+
+		for (unsigned int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
+
+			if (hardware_read_order(i, HARDWARE_ORDER_UP)) {
+				
+			}
+
+			if (hardware_read_order(i, HARDWARE_ORDER_INSIDE)) {
+
+			}
+
+			if (hardware_read_order(i, HARDWARE_ORDER_DOWN)) {
+
+			}
+		}
+    }
+
+	return 0;
 }
 
 void fsmUpdateLights() {
@@ -22,16 +57,12 @@ State fsmDecideNextState(State currentState) {
 
 	switch (currentState) {
 		case Startup:
-			bool isAtFloor = true;
 
 			for (unsigned int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
-				isAtFloor = hardware_read_floor_sensor(i);
+				if (hardware_read_floor_sensor(i)) {
+					nextState = Idle;
+				}
 			}
-			
-
-			if (!isAtFloor) {
-				hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-			}	
 
 		break;
 
@@ -59,48 +90,8 @@ State fsmDecideNextState(State currentState) {
 }
 
 void fsmTransition(State currentState, State nextState) {
-	// Kode her
-	return;
-}
 
-void fsmStateEnter(State currentState) {
-	switch (currentState) {
-		case Startup:
-			bool isAtFloor = true;
-
-			for (unsigned int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
-				isAtFloor = hardware_read_floor_sensor(i);
-			}
-			
-
-			if (!isAtFloor) {
-				hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-			}	
-
-		break;
-
-		case Idle:
-
-		break;
-
-		case Move:
-
-		break;
-
-		case DoorOpen:
-
-		break;
-		
-		case Stop:
-
-		break;
-
-		default:
-		break;
-	}
-}
-
-void fsmStateExit(State currentState) {
+	// Perform exit for current state	
 	switch (currentState) {
 		case Startup:
 			hardware_command_movement(HARDWARE_MOVEMENT_STOP);
@@ -125,10 +116,68 @@ void fsmStateExit(State currentState) {
 		default:
 		break;
 	}
-	return;
+
+	// Perform enter for next state	
+	switch (nextState) {
+		case Startup:
+			bool isAtFloor = true;
+
+			for (unsigned int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
+				isAtFloor = hardware_read_floor_sensor(i);
+			}
+
+			if (!isAtFloor) {
+				hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+			}	
+
+		break;
+
+		case Idle:
+			// No enter
+		break;
+
+		case Move:
+
+		break;
+
+		case DoorOpen:
+
+		break;
+		
+		case Stop:
+
+		break;
+
+		default:
+		break;
+	}
 }
 
 void fsmStateUpdate(State currentState) {
-	// Kode her
+
+	switch (currentState) {
+		case Startup:
+			// Ignore enqueuements
+		break;
+
+		case Idle:
+			
+		break;
+
+		case Move:
+
+		break;
+
+		case DoorOpen:
+
+		break;
+		
+		case Stop:
+
+		break;
+
+		default:
+		break;
+	}
 	return;
 }
