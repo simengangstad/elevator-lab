12d06aec44cc7c3ae9888237315f63c5152c6da6
Simen Gangstad <simen.gangstad@me.com>
2020-02-22 02:05:30 +0100

    Remove fsmUpdateLights

--- source/fsm.h
+++ source/fsm.h
@@ -26,20 +26,16 @@ typedef enum {
 
 
 /**
- * @brief Updates all lights connected to the elevator.
- */
-void fsmUpdateLights();
-
-/**
  * @brief Polls relevant hardware and decides next state.
  *
  * @param[in] currentState Current state of the elevator
  * @param[in] priortyQueue The current queue, makes it possible for states to check if 
  * 						   the queue is in a given state in order to decide the next state. 
- *
+ * @param[in] currentFloor The current floor the elevator is at. If the elevator is between floors this argument 
+ * 						   should be the last floor the elevator was at. 
  * @return Next state in the fsm based on current state of hardware.
  */
-State fsmDecideNextState(State currentState, const Node* priorityQueue);
+State fsmDecideNextState(State currentState, const Node* priorityQueue, const int currentFloor);
 
 /**
  * @brief Handles transitioning between two states, @p currentState and @p nextState. Will execute the exit operations for @p currentState 
@@ -47,8 +43,10 @@ State fsmDecideNextState(State currentState, const Node* priorityQueue);
  *
  * @param[in] currentState Current state of the elevator, the state the fsm is leaving.
  * @param[in] nextState Next state of the elevator, the state the fsm is entering.
+ * @param[in] priorityQueue The current queue, used to check the current state for the orders
+ * 							during the transition and update the queue. 
  */
-void fsmTransition(State currentState, State nextState);
+void fsmTransition(State currentState, State nextState, Node* priorityQueue);
 
 /**
  * @brief Will execute the update function defined for the @p currentState and update
