b7f9a6953a316d63927aa05771bd0d3c05dc211c
Simen Gangstad <simen.gangstad@me.com>
2020-02-22 01:03:24 +0100

    Remove enter and exit, is now a part of transition

--- source/fsm.h
+++ source/fsm.h
@@ -5,7 +5,12 @@
 
 #ifndef FSM_H
 #define FSM_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
 #include "hardware.h"
+#include "priority_queue.h"
 
 /**
  * @brief Enum of the possible defined states
@@ -21,13 +26,6 @@ typedef enum {
 
 
 /**
- * @brief Performs "Do"-actions connected to @p currentState
- *
- * @param[in] currentState Current state of the elevator.
- */
-void fsmUpdate(State currentState);
-
-/**
  * @brief Updates all lights connected to the elevator.
  */
 void fsmUpdateLights();
@@ -36,13 +34,16 @@ void fsmUpdateLights();
  * @brief Polls relevant hardware and decides next state.
  *
  * @param[in] currentState Current state of the elevator
+ * @param[in] priortyQueue The current queue, makes it possible for states to check if 
+ * 						   the queue is in a given state in order to decide the next state. 
  *
  * @return Next state in the fsm based on current state of hardware.
  */
-State fsmDecideNextState(State currentState);
+State fsmDecideNextState(State currentState, const Node* priorityQueue);
 
 /**
- * @brief Handles transitioning between two states, @p currentState and @p nextState.
+ * @brief Handles transitioning between two states, @p currentState and @p nextState. Will execute the exit operations for @p currentState 
+ * 		  and the enter operations for @p nextState.
  *
  * @param[in] currentState Current state of the elevator, the state the fsm is leaving.
  * @param[in] nextState Next state of the elevator, the state the fsm is entering.
@@ -50,23 +51,12 @@ State fsmDecideNextState(State currentState);
 void fsmTransition(State currentState, State nextState);
 
 /**
- * @brief Performs "Enter"-actions connected to entering @p currentState .
- *
- * @param[in] currentState State that is being entered.
- */
-void fsmStateEnter(State currentState);
-
-/**
- * @brief Performs "Exit"-actions connected to exiting @p currentState.
- *
- * @param[in] currentState State that is being exited out of.
- */
-void fsmStateExit(State currentState);
-
-/**
  * @brief Will execute the update function defined for the @p currentState and update
  * 		  internal state.
+ * 
+ * @param[in] shouldClearOrders Makes it possible for states to clear the queue and prevent further enqueuements.
+ * 							    This is utilized by the #Startup and #Stop state. 
  */
-void fsmStateUpdate(State currentState);
+void fsmStateUpdate(State currentState, bool* shouldClearOrders);
 
 #endif
