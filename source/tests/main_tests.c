3b91f51599c5f03d88116ccfc07db7eab47e0153
Simen Gangstad <simen.gangstad@me.com>
2020-02-24 19:39:22 +0100

    Remove main function

--- source/tests/main_tests.c
+++ source/tests/main_tests.c
@@ -3,29 +3,31 @@
  * @brief Entry testing for module testing.
  */
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include "../hardware.h"
 #include "door_tests.h"
 #include "fsm_tests.h"
-#include "../hardware.h"
 
-static void sigint_handler(int sig){
-    (void)(sig);
-    printf("Terminating elevator\n");
-    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-    exit(0);
-}
+static void sigint_handler(int sig);
 
-int main(){
+void test() {
     int error = hardware_init();
-    if(error != 0){
+    if (error != 0) {
         fprintf(stderr, "Unable to initialize hardware\n");
         exit(1);
     }
 
     signal(SIGINT, sigint_handler);
-	
-	doorTestsValidate();
+
+    doorTestsValidate();
     fsmTestsValidate();
+}
+
+static void sigint_handler(int sig) {
+    (void)(sig);
+    printf("Terminating elevator\n");
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+    exit(0);
 }
