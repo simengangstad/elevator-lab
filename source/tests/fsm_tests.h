9e6e649867bd26ef48cb2cb3ba8c848d83cab694
Simen Gangstad <simen.gangstad@me.com>
2020-02-25 08:41:24 +0100

    Add FSM tests

---
+++ source/tests/fsm_tests.h
@@ -0,0 +1,16 @@
+/**
+ * @file 
+ * 
+ * @brief Test for the FSM module. 
+ */
+
+#ifndef FSM_TESTS_H
+#define FSM_TESTS_H
+
+#include <stdbool.h>
+#include "../fsm.h"
+#include "test_util.h"
+
+void fsmTestsValidate();
+
+#endif
