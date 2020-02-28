d44b5d68bfd1129ad61540c94057f8b33544bfc3
Simen Gangstad <simen.gangstad@me.com>
2020-02-28 11:41:01 +0100

    Move function declarations to top

--- source/fsm.c
+++ source/fsm.c
@@ -4,6 +4,91 @@
 
 #include "fsm.h"
 
+static void sigint_handler(int sig);
+
+const char* get_state_name(const State state);
+
+const char* get_movement_name(const HardwareMovement movement);
+
+// TODO: IS this at bit to unreadable?
+
+/**
+ * @brief Checks if the elevator is currently at a floor.
+ * 
+ * @param [out] current_floor Will be overwritten with the floor number if the elevator is at a given floor.
+ * 
+ * @return True of the elevator is at a floor.
+ */
+static bool fsm_elevator_is_at_floor(int* current_floor);
+
+/**
+ * @brief Checks where the elevator is given @p movement.
+ * 
+ * @param [in] last_floor The floor the elevator was last recorded to be at.
+ * @param [in] movement The current movement of the elevator.
+ * 
+ * @return The position of the elevator.
+ */
+static Position fsm_decide_position(const int last_floor, const HardwareMovement movement);
+
+/**
+ * @brief Polls the current orders and puts them in the @p p_priority_queue.
+ * 
+ * @param [in] p_priority_queue The current queue.
+ * @param [in] current_position The position of the elevator, used in the queue algorithm to decide where the new orders
+ *                              should be placed.
+ * 
+ * @return The updated queue.
+ */
+static Node* fsm_poll_orders_and_update_queue(Node* p_priority_queue, const Position current_position);
+
+int main() {
+    int error = hardware_init();
+    if (error != 0) {
+        fprintf(stderr, "Unable to initialize hardware\n");
+        exit(1);
+    }
+
+    signal(SIGINT, sigint_handler);
+
+    State current_state = STATE_UNDEFINED;
+    int last_floor = FLOOR_UNDEFINED;
+    Position current_position = {FLOOR_UNDEFINED, OFFSET_UNDEFINED};
+    Node* p_priority_queue = NULL;
+    HardwareMovement* p_last_movement = malloc(sizeof(HardwareMovement));
+    *p_last_movement = HARDWARE_MOVEMENT_STOP;
+
+    while (1) {
+        current_position = fsm_decide_position(last_floor, *p_last_movement);
+
+        if (current_position.floor != FLOOR_UNDEFINED && current_position.offset == OFFSET_AT_FLOOR) {
+            hardware_command_floor_indicator_on(current_position.floor);
+            last_floor = current_position.floor;
+        }
+
+        State next_state = fsm_decide_next_state(current_state, p_priority_queue, current_position);
+
+        if (next_state != current_state) {
+            fsm_transition(current_state, next_state, &p_priority_queue, p_last_movement, current_position);
+            current_state = next_state;
+        }
+
+        fsm_state_update(current_state, &p_priority_queue, current_position);
+        door_update();
+
+        printf("CURRENT STATE: %s\n", get_state_name(current_state));
+        printf("CURRENT FLOOR: %d\n", current_position.floor);
+        printf("LAST MOVEMENT: %s\n", get_movement_name(*p_last_movement));
+        queue_print(p_priority_queue);
+        printf("=================\n");
+    }
+
+    free(p_last_movement);
+    queue_clear(p_priority_queue);
+
+    return 0;
+}
+
 static void sigint_handler(int sig) {
     (void)(sig);
     printf("Terminating elevator\n");
@@ -11,7 +96,7 @@ static void sigint_handler(int sig) {
     exit(0);
 }
 
-const char *get_state_name(const State state) {
+const char* get_state_name(const State state) {
     switch (state) {
         case STATE_STARTUP:
             return "Startup";
@@ -30,7 +115,7 @@ const char *get_state_name(const State state) {
     }
 }
 
-const char *get_movement_name(const HardwareMovement movement) {
+const char* get_movement_name(const HardwareMovement movement) {
     switch (movement) {
         case HARDWARE_MOVEMENT_UP:
             return "Up";
@@ -43,16 +128,7 @@ const char *get_movement_name(const HardwareMovement movement) {
     }
 }
 
-// TODO: IS this at bit to unreadable?
-
-/**
- * @brief Checks if the elevator is currently at a floor.
- * 
- * @param [out] current_floor Will be overwritten with the floor number if the elevator is at a given floor.
- * 
- * @return True of the elevator is at a floor.
- */
-static bool fsm_is_at_floor(int *current_floor) {
+static bool fsm_elevator_is_at_floor(int* current_floor) {
     for (unsigned int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
         if (hardware_read_floor_sensor(floor)) {
             *current_floor = floor;
@@ -60,25 +136,17 @@ static bool fsm_is_at_floor(int *current_floor) {
         }
     }
 
-    *current_floor = -1;
+    *current_floor = FLOOR_UNDEFINED;
 
     return false;
 }
 
-/**
- * @brief Checks where the elevator is given @p movement.
- * 
- * @param [in] last_floor The floor the elevator was last recorded to be at.
- * @param [in] movement The current movement of the elevator.
- * 
- * @return The position of the elevator.
- */
 static Position fsm_decide_position(const int last_floor, const HardwareMovement movement) {
     int new_floor = last_floor;
     Offset offset = OFFSET_UNDEFINED;
 
     int current_floor = last_floor;
-    if (fsm_is_at_floor(&current_floor)) {
+    if (fsm_elevator_is_at_floor(&current_floor)) {
         new_floor = current_floor;
         offset = OFFSET_AT_FLOOR;
     }
@@ -92,84 +160,27 @@ static Position fsm_decide_position(const int last_floor, const HardwareMovement
     return (Position){new_floor, offset};
 }
 
-int main() {
-    int error = hardware_init();
-    if (error != 0) {
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
-    }
-
-    signal(SIGINT, sigint_handler);
-
-    State current_state = STATE_STARTUP;
-    Node *p_priority_queue = NULL;
-
-    // TODO: remove these and use functions for updating orders
-    bool *p_should_clear_orders = malloc(sizeof(bool));
-    *p_should_clear_orders = false;
-
-    int last_floor = -1;
-    Position current_position = {-1, OFFSET_UNDEFINED};
-
-    HardwareMovement *p_last_movement = malloc(sizeof(HardwareMovement));
-    *p_last_movement = HARDWARE_MOVEMENT_STOP;
-
-    // Transition to the start up state so we enter it properly before the main loop, this will just ensure
-    // that the code associated with the start up state is executed
-    fsm_transition(STATE_UNDEFINED, STATE_STARTUP, &p_priority_queue, p_last_movement, current_position);
-
-    while (1) {
-        current_position = fsm_decide_position(last_floor, *p_last_movement);
-
-        if (current_position.offset == OFFSET_AT_FLOOR) {
-            last_floor = current_position.floor;
-            hardware_command_floor_indicator_on(current_position.floor);
-        }
-
-        State next_state = fsm_decide_next_state(current_state, p_priority_queue, current_position);
-
-        if (next_state != current_state) {
-            fsm_transition(current_state, next_state, &p_priority_queue, p_last_movement, current_position);
-            current_state = next_state;
-            *p_should_clear_orders = false;
-        }
-
-        fsm_state_update(current_state, current_position.floor, p_should_clear_orders);
-        door_update();
-
-        if (!*p_should_clear_orders) {
-            if (current_position.floor != -1) {
-                for (unsigned int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
-                    for (HardwareOrder order_type = HARDWARE_ORDER_UP; order_type <= HARDWARE_ORDER_DOWN; order_type++) {
-                        if (hardware_read_order(floor, order_type)) {
-                            printf("hello");
-                            p_priority_queue = queue_add_node(node_create(floor, order_type), p_priority_queue, current_position.floor);
-                            hardware_command_order_light(floor, order_type, true);
-                        }
-                    }
-                }
+static Node* fsm_poll_orders_and_update_queue(Node* p_priority_queue, const Position current_position) {
+    for (unsigned int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
+        for (HardwareOrder order_type = HARDWARE_ORDER_UP; order_type <= HARDWARE_ORDER_DOWN; order_type++) {
+            if (hardware_read_order(floor, order_type)) {
+                p_priority_queue = queue_add_node(node_create(floor, order_type), p_priority_queue, current_position.floor);
+                hardware_command_order_light(floor, order_type, true);
             }
-        } else {
-            p_priority_queue = queue_clear(p_priority_queue);
         }
-
-        printf("CURRENT STATE: %s\n", get_state_name(current_state));
-        printf("CURRENT FLOOR: %d\n", current_position.floor);
-        printf("LAST MOVEMENT: %s\n", get_movement_name(*p_last_movement));
-        queue_print(p_priority_queue);
-        printf("=================\n");
     }
 
-    free(p_should_clear_orders);
-    free(p_last_movement);
-
-    return 0;
+    return p_priority_queue;
 }
 
-State fsm_decide_next_state(const State current_state, const Node *p_priority_queue, const Position current_position) {
+State fsm_decide_next_state(const State current_state, const Node* p_priority_queue, const Position current_position) {
     State next_state = current_state;
 
     switch (current_state) {
+        case STATE_UNDEFINED:
+            next_state = STATE_STARTUP;
+            break;
+
         case STATE_STARTUP: {
             if (hardware_read_stop_signal()) {
                 next_state = STATE_STOP;
@@ -224,7 +235,7 @@ State fsm_decide_next_state(const State current_state, const Node *p_priority_qu
     return next_state;
 }
 
-void fsm_transition(const State current_state, const State next_state, Node **pp_priority_queue, HardwareMovement *p_last_movement, const Position current_position) {
+void fsm_transition(const State current_state, const State next_state, Node** pp_priority_queue, HardwareMovement* p_last_movement, const Position current_position) {
     // Perform exit for current state
     switch (current_state) {
         case STATE_STARTUP: {
@@ -261,9 +272,9 @@ void fsm_transition(const State current_state, const State next_state, Node **pp
             for (unsigned int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
                 if (hardware_read_floor_sensor(floor)) {  // Her itererer vi likevel over alle etasjene, hvorfor ikke sette current_floor her? -> Sentralisering av setting av globale var.
                     is_at_floor = true;
-                    hardware_command_floor_indicator_on(floor);
                 }
 
+                // Clear all existing order lights
                 for (HardwareOrder order_type = HARDWARE_ORDER_UP; order_type <= HARDWARE_ORDER_DOWN; order_type++) {  // Hvorfor er dette med?
                     hardware_command_order_light(floor, order_type, 0);
                 }
@@ -321,23 +332,22 @@ void fsm_transition(const State current_state, const State next_state, Node **pp
     }
 }
 
-void fsm_state_update(const State current_state, const int current_floor, bool *p_should_clear_orders) {
+void fsm_state_update(const State current_state, Node** pp_priority_queue, const Position current_position) {
     switch (current_state) {
         case STATE_STARTUP: {
-            *p_should_clear_orders = true;
+            *pp_priority_queue = queue_clear(*pp_priority_queue);
         } break;
 
         case STATE_IDLE: {
-            // No Update
+            *pp_priority_queue = fsm_poll_orders_and_update_queue(*pp_priority_queue, current_position);
         } break;
 
         case STATE_MOVE: {
-            hardware_command_floor_indicator_on(current_floor);
-            // No update
+            *pp_priority_queue = fsm_poll_orders_and_update_queue(*pp_priority_queue, current_position);
         } break;
 
         case STATE_DOOR_OPEN: {
-            // No update
+            *pp_priority_queue = fsm_poll_orders_and_update_queue(*pp_priority_queue, current_position);
         } break;
 
         case STATE_STOP: {
@@ -347,7 +357,7 @@ void fsm_state_update(const State current_state, const int current_floor, bool *
                 }
             }
 
-            *p_should_clear_orders = true;  // Eneste stedet vi clearer ordre, hvorfor ikke bare gjøre her? Kalle queue_clear? -> unngår å i det hele tatt polle etasjeknapper
+            *pp_priority_queue = queue_clear(*pp_priority_queue);
         } break;
 
         default:
