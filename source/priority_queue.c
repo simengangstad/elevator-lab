779ca9cdae89a14344cb7cea7badee4aa1ff2371
Simen Gangstad <simen.gangstad@me.com>
2020-02-28 11:40:16 +0100

    Update comments

--- source/priority_queue.c
+++ source/priority_queue.c
@@ -5,9 +5,9 @@
 
 #include "priority_queue.h"
 
-Node *node_create(const int floor, const HardwareOrder direction) {
-    Node *p_new_node = NULL;
-    p_new_node = (Node *)malloc(sizeof(Node));
+Node* node_create(const int floor, const HardwareOrder direction) {
+    Node* p_new_node = NULL;
+    p_new_node = (Node*)malloc(sizeof(Node));
 
     p_new_node->floor = floor;
     p_new_node->direction = direction;
@@ -16,12 +16,12 @@ Node *node_create(const int floor, const HardwareOrder direction) {
     return p_new_node;
 }
 
-Node *queue_add_node(Node *p_new_node, Node *p_first_node_of_queue, const int current_floor) {
+Node* queue_add_node(Node* p_new_node, Node* p_first_node_of_queue, const int current_floor) {
     if (!p_new_node) {
         return p_first_node_of_queue;
     }
 
-    Node *p_new_first_node_of_queue = NULL;
+    Node* p_new_first_node_of_queue = NULL;
 
     // Algorithm for adding node to queue
     if (queue_is_empty(p_first_node_of_queue)) {
@@ -36,13 +36,13 @@ Node *queue_add_node(Node *p_new_node, Node *p_first_node_of_queue, const int cu
             p_new_node->next_node = p_first_node_of_queue;
             p_new_first_node_of_queue = p_new_node;
         }
-        // If the new order is not on the way to the destination, put it at the bottom of the queue
+        // If the new order is *not* on the way to the destination, put it at the bottom of the queue
         else {
             p_new_node->next_node = NULL;
             p_new_first_node_of_queue = p_first_node_of_queue;
 
             // Traverse the queue and place the new order at the bottom
-            Node *p_current_node = p_new_first_node_of_queue;
+            Node* p_current_node = p_new_first_node_of_queue;
             while (p_current_node->next_node) {
                 p_current_node = p_current_node->next_node;
             }
@@ -51,8 +51,8 @@ Node *queue_add_node(Node *p_new_node, Node *p_first_node_of_queue, const int cu
 
         // Traverse queue, delete orders to duplicate floors
         bool order_on_floor[4] = {false, false, false, false};
-        Node *p_current_node = p_new_first_node_of_queue;
-        Node *p_previous_node = NULL;
+        Node* p_current_node = p_new_first_node_of_queue;
+        Node* p_previous_node = NULL;
 
         while (p_current_node) {
             if (order_on_floor[p_current_node->floor]) {
@@ -70,7 +70,7 @@ Node *queue_add_node(Node *p_new_node, Node *p_first_node_of_queue, const int cu
     return p_new_first_node_of_queue;
 }
 
-Node *queue_pop(Node *p_first_node_of_queue, int current_floor) {
+Node* queue_pop(Node* p_first_node_of_queue, int current_floor) {
     if (!p_first_node_of_queue) {
         return NULL;
     }
@@ -81,16 +81,16 @@ Node *queue_pop(Node *p_first_node_of_queue, int current_floor) {
         return NULL;
     }
 
-    Node *p_new_first_node_of_queue = p_first_node_of_queue->next_node;
+    Node* p_new_first_node_of_queue = p_first_node_of_queue->next_node;
     free(p_first_node_of_queue);
 
     // Check if orders on the bottom of the queue are compatible with the new goal by adding them back to the queue
-    Node *p_remaining_nodes = p_new_first_node_of_queue->next_node;
-    Node *p_remaining_nodes_copy = p_remaining_nodes;
+    Node* p_remaining_nodes = p_new_first_node_of_queue->next_node;
+    Node* p_remaining_nodes_copy = p_remaining_nodes;
     p_new_first_node_of_queue->next_node = NULL;
 
     while (p_remaining_nodes) {
-        Node *p_temp_node = node_create(p_remaining_nodes->floor, p_remaining_nodes->direction);
+        Node* p_temp_node = node_create(p_remaining_nodes->floor, p_remaining_nodes->direction);
         p_new_first_node_of_queue = queue_add_node(p_temp_node, p_new_first_node_of_queue, current_floor);
         p_remaining_nodes = p_remaining_nodes->next_node;
     }
@@ -100,10 +100,10 @@ Node *queue_pop(Node *p_first_node_of_queue, int current_floor) {
     return p_new_first_node_of_queue;
 }
 
-Node *queue_clear(Node *p_first_node_of_queue) {
-    Node *p_traverse_node = p_first_node_of_queue;
+Node* queue_clear(Node* p_first_node_of_queue) {
+    Node* p_traverse_node = p_first_node_of_queue;
     while (p_traverse_node) {
-        Node *p_temp_node = p_traverse_node;
+        Node* p_temp_node = p_traverse_node;
         p_traverse_node = p_traverse_node->next_node;
         free(p_temp_node);
     }
@@ -111,10 +111,10 @@ Node *queue_clear(Node *p_first_node_of_queue) {
     return NULL;
 }
 
-bool queue_is_empty(const Node *p_first_node_of_queue) { return !p_first_node_of_queue; }
+bool queue_is_empty(const Node* p_first_node_of_queue) { return !p_first_node_of_queue; }
 
-void queue_print(Node *p_first_node_of_queue) {
-    Node *p_temp_node = p_first_node_of_queue;
+void queue_print(Node* p_first_node_of_queue) {
+    Node* p_temp_node = p_first_node_of_queue;
     int n = 1;
     while (p_temp_node) {
         printf("%i", n);
