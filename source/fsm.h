d818383c4b57666dd480a15657a83a1c127ad0ac
Simen Gangstad <simen.gangstad@me.com>
2020-03-01 22:09:56 +0100

    Move Position to separate file

--- source/fsm.h
+++ source/fsm.h
@@ -13,6 +13,7 @@
 #define FLOOR_UNDEFINED -1
 
 #include "hardware.h"
+#include "position.h"
 #include "priority_queue.h"
 
 // TODO: Liker egentlig ikke at dette er public, men det må jo være det for testene...
@@ -28,34 +29,6 @@ typedef enum {
     STATE_STOP,
     STATE_UNDEFINED
 } State;
-
-/**
- * @brief Specifies where the elevator is relative to a given floor.
- */
-typedef enum Offset {
-    OFFSET_AT_FLOOR,
-    OFFSET_BELOW,
-    OFFSET_ABOVE,
-    OFFSET_UNDEFINED
-} Offset;
-
-/**
- * @brief Information about where the elevator is in relation to a given floor. 
- * 
- * @note If the elevator is between the 3rd and the 2nd floor going down, this 
- *       struct would hold 3rd floor at OFFSET_BELOW from the #Offset enum.
- */
-typedef struct Position {
-    /**
-     * @brief The floor of the position.
-     */
-    int floor;
-
-    /**
-     * @brief The offset of the position.
-     */
-    Offset offset;
-} Position;
 
 /**
  * @brief Starts the FSM.
