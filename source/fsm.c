95fa923268ee36dc96863ac954c07e0e4cf4beef
Simen Gangstad <simen.gangstad@me.com>
2020-03-01 22:27:41 +0100

    Move functions to .c file

--- source/fsm.c
+++ source/fsm.c
@@ -11,6 +11,62 @@
 #include <stdlib.h>
 
 #include "door.h"
+#include "hardware.h"
+#include "position.h"
+#include "priority_queue.h"
+
+/**
+ * @brief The possible states for the state machine.
+ */
+typedef enum {
+    STATE_STARTUP,
+    STATE_IDLE,
+    STATE_MOVE,
+    STATE_DOOR_OPEN,
+    STATE_STOP,
+    STATE_UNDEFINED
+} State;
+
+/**
+ * @brief Polls hardware, checks the current state and queue, and decides the next state based.
+ *
+ * @param[in] current_state Current state of the elevator
+ * @param[in] p_priority_queue The current queue, makes it possible for states to check if 
+ * 						     the queue is in a given state in order to decide the next state. 
+ * @param[in] current_position The current position the elevator is at. 
+ * 
+ * @return Next state in the FSM based on @p current_state, @p priority_queue, @p current_position and hardware input.
+ */
+static State fsm_decide_next_state(const State current_state,
+                                   const Order* p_priority_queue,
+                                   const Position current_position);
+
+/**
+ * @brief Handles transitioning between two states, @p current_state and @p next_state. Will execute the exit 
+ *        operations for @p current_state and the enter operations for @p next_state.
+ *
+ * @param[in] current_state Current state of the elevator, the state the FSM is leaving.
+ * @param[in] next_state Next state of the elevator, the state the FSM is entering.
+ * @param[in, out] pp_priority_queue Pointer to the current queue, used to check the current state for the orders
+ * 							   during the transition and for updating the queue. 
+ * @param[in, out] p_movement_when_left_floor The movement the elevator was set to when it left from the last floor. 
+ * @param[in] current_position The current position of the elevator. 
+ */
+static void fsm_transition(const State current_state,
+                           const State next_state,
+                           Order** pp_priority_queue,
+                           HardwareMovement* p_movement_when_left_floor,
+                           const Position current_position);
+
+/**
+ * @brief Will execute the update function defined for the @p current_state and update
+ * 		  the @p pp_priority_queue.
+ * 
+ * @param[in] current_state The current state that shall have its update function called. 
+ * @param[in, out] pp_priority_queue The queue, makes it possible for the states to update the queue. 
+ * @param[in] current_position The current position of the elevator. 
+ */
+static void fsm_state_update(const State current_state, Order** pp_priority_queue, const Position current_position);
 
 /**
  * @brief Gets the floor the elevator is currently at.
--- source/fsm.h
+++ source/fsm.h
@@ -12,62 +12,9 @@
  */
 #define FLOOR_UNDEFINED -1
 
-#include "hardware.h"
-#include "position.h"
-#include "priority_queue.h"
-
-// TODO: Liker egentlig ikke at dette er public, men det må jo være det for testene...
-
-/**
- * @brief The possible states for the state machine.
- */
-typedef enum {
-    STATE_STARTUP,
-    STATE_IDLE,
-    STATE_MOVE,
-    STATE_DOOR_OPEN,
-    STATE_STOP,
-    STATE_UNDEFINED
-} State;
-
 /**
  * @brief Starts the FSM.
  */
 void fsm_run();
-
-/**
- * @brief Polls hardware, checks the current state and queue, and decides the next state based.
- *
- * @param[in] current_state Current state of the elevator
- * @param[in] p_priority_queue The current queue, makes it possible for states to check if 
- * 						     the queue is in a given state in order to decide the next state. 
- * @param[in] current_position The current position the elevator is at. 
- * 
- * @return Next state in the FSM based on @p current_state, @p priority_queue, @p current_position and hardware input.
- */
-State fsm_decide_next_state(const State current_state, const Order* p_priority_queue, const Position current_position);
-
-/**
- * @brief Handles transitioning between two states, @p current_state and @p next_state. Will execute the exit operations for @p current_state 
- * 		  and the enter operations for @p next_state.
- *
- * @param[in] current_state Current state of the elevator, the state the FSM is leaving.
- * @param[in] next_state Next state of the elevator, the state the FSM is entering.
- * @param[in, out] pp_priority_queue Pointer to the current queue, used to check the current state for the orders
- * 							   during the transition and for updating the queue. 
- * @param[in, out] p_movement_when_left_floor The movement the elevator was set to when it left from the last floor. 
- * @param[in] current_position The current position of the elevator. 
- */
-void fsm_transition(const State current_state, const State next_state, Order** pp_priority_queue, HardwareMovement* p_movement_when_left_floor, const Position current_position);
-
-/**
- * @brief Will execute the update function defined for the @p current_state and update
- * 		  the @p pp_priority_queue.
- * 
- * @param[in] current_state The current state that shall have its update function called. 
- * @param[in, out] pp_priority_queue The queue, makes it possible for the states to update the queue. 
- * @param[in] current_position The current position of the elevator. 
- */
-void fsm_state_update(const State current_state, Order** pp_priority_queue, const Position current_position);
 
 #endif
